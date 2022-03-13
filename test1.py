386c7ef27ddd93e555fbe8d8120236061a0460dd
cuiwenqi <cuiwenqicn@gmail.com>
2022-03-13 21:41:58 +0800

    the first commit

---
+++ test1.py
@@ -0,0 +1,21 @@
+#!usr/bin/python3
+print("hello world")
+
+counter = 100
+t = ['a','b','c','d','e']
+print (counter)
+print (t[1:3])
+dict = {}
+dict[2]="This is two"
+a=10
+b=20
+c=a+b
+print("c =",c)
+
+flag=False
+name='luren'
+if name == 'python':
+	flag = True
+	print ('welcome boss')
+else:
+	print (name)
